7dadff2649bb1cd8c4c8ae28c482569e66bb244e
sudo-m3zim <nizilplay2004@gmail.com>
2024-05-05 23:44:12 +0300

    Maded tokenizer.

--- Language/token.cpp
+++ Language/token.cpp
@@ -14,15 +14,45 @@ Token::Token(std::string word)
 
 Token::~Token()
 {
-	
+	return;
 }
 
 void Token::generate_accepted_tokens(std::string word)
 {
-	if (strcmp(word.c_str(), "+"))
+	if (word == "+")
+	{
+		type = PlusToken;
+		value = 0;
+		accepted_tokens.push_back(DigidToken);
 		return;
-	if (strcmp(word.c_str(), "-"))
+	}
+	if (word == "-")
+	{
+		type = MinusToken;
+		value = 0;
+		accepted_tokens.push_back(DigidToken);
 		return;
-	if (strcmp(word.c_str(), "="))
+	}
+	if (word == "=")
+	{
+		type = EqualToken;
+		value = 0;
+		accepted_tokens.push_back(DigidToken);
 		return;
+	}
+	type = DigidToken;
+	value = stoi(word);
+	accepted_tokens.push_back(PlusToken);
+	accepted_tokens.push_back(MinusToken);
+	accepted_tokens.push_back(EqualToken);
+}
+
+bool Token::is_token_accepted(Token token)
+{
+	for (int token_index = 0; token_index < accepted_tokens.size(); token_index++)
+	{
+		if (token.type == accepted_tokens[token_index])
+			return true;
+	}
+	return false;
 }
